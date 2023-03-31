ecd0209ed89a169a2510583514ac4d2515818317
jiro141 <felipe.colmenaresvzla@gmail.com>
2023-03-31 11:41:32 -0400

    patolsima

---
+++ src/components/Sidebar/Calendario.js
@@ -0,0 +1,22 @@
+import React, { useState } from 'react';
+import Calendar from 'react-calendar';
+import '../../css/style.css';
+
+export function Calendario() {
+  const [date, setDate] = useState(new Date());
+
+  const formatShortYear = (locale, date) => {
+    return date.getFullYear();
+  }
+
+  return (
+    <div>
+      <Calendar
+        value={date}
+        onChange={setDate}
+        formatShortYear={formatShortYear}
+      />
+    </div>
+  );
+}
+export default Calendario
---
+++ src/routes.js
@@ -0,0 +1,74 @@
+﻿// import
+import Dashboard from "views/Dashboard/Dashboard";
+import Billing from "views/Dashboard/Billing";
+import RTLPage from "views/Dashboard/RTL";
+import Profile from "views/Dashboard/Profile";
+import SignIn from "views/Auth/SignIn.js";
+import SignUp from "views/Auth/SignUp.js";
+import Registro from "views/Dashboard/Registro"
+import Muestras from "views/Dashboard/Muestras"
+import Prueba from "views/Dashboard/Prueba"
+
+import {
+  HomeIcon,
+  StatsIcon,
+  CreditIcon,
+  PersonIcon,
+  DocumentIcon,
+  RocketIcon,
+  SupportIcon,
+} from "components/Icons/Icons";
+
+var dashRoutes = [
+  {
+    path: "/dashboard",
+    name: "Inicio",
+    icon: <HomeIcon color="inherit" />,
+    component: Dashboard,
+    layout: "/admin",
+  },
+  {
+    path: "/Registro",
+    name: "Registro",
+    rtlName: "لوحة القيادة",
+    icon: <StatsIcon color="inherit" />,
+    component: Registro,
+    layout: "/admin",
+  },
+  {
+    path: "/Muestras",
+    name: "Muestras",
+    rtlName: "لوحة القيادة",
+    icon: <CreditIcon color="inherit" />,
+    component: Prueba,
+    layout: "/admin",
+  },
+  
+  {
+    name: "Patólogia",
+    category: "account",
+    rtlName: "صفحات",
+    state: "pageCollapse",
+    views: [
+      {
+        path: "/SignUp",
+        name: "SingUp",
+        rtlName: "لوحة القيادة",
+        icon: <PersonIcon color="inherit" />,
+        secondaryNavbar: true,
+        component: SignUp,
+        layout: "/auth",
+      },
+      {
+        path: "/signin",
+        name: "Sign In",
+        rtlName: "لوحة القيادة",
+        icon: <DocumentIcon color="inherit" />,
+        component: SignIn,
+        layout: "/auth",
+      },
+      
+    ],
+  },
+];
+export default dashRoutes;
